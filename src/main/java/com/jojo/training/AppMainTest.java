bc61aec802e2154eae3e42820f1d8ca99e792152
jinxin <jinxin@it.lianfa.cn>
2023-06-08 13:45:10 +0800

    log打印优化

--- src/main/java/com/jojo/training/AppMainTest.java
+++ src/main/java/com/jojo/training/AppMainTest.java
@@ -47,14 +47,12 @@ public class AppMainTest {
 
     public static void main(String[] args) {
         int lengthOfLongestSubstring = lengthOfLongestSubstring("dvdf");
-        ;
         ListNode l1 = new ListNode(9, new ListNode(9));
         ListNode l2 = new ListNode(9, new ListNode(9, new ListNode(9)));
         ListNode addTwoNumbers = addTwoNumbers(l1, l2);
 
-        System.out.println("hello");
         log.log(Level.INFO, "Hello world! {0}", String.valueOf(lengthOfLongestSubstring));
-        log.log(Level.INFO, "Hello world! {0}", addTwoNumbers.toString());
+        log.log(Level.INFO, "Hello world! {0}", addTwoNumbers);
     }
 }
 
